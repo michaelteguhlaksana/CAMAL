3a76bdb6c8d53894c76f132f5e04654ae8f773b7
michaelteguhlaksana <michaelteguhlaksana@gmail.com>
2022-10-26 00:36:12 +0800

    Added CascadeSeries class

--- scripts/camal.py
+++ scripts/camal.py
@@ -120,6 +120,111 @@ class Series(object):
 		return reg.score(self.x, y), reg.coef_, reg.intercept_, -reg.intercept_/reg.coef_
 
 
+class CascadeSeries(object):
+	"""
+	Several series bundled together to allow different windows and regions all at once
+	Assuming all series came from the same set of x
+	All series are assumed to be contiguous. Any separation in the series should be separated to 2 series in the args, with the same name
+	"""
+	def __init__(self, x, series):
+		self.x = x
+		self.series = {}
+		self.hash_index = []
+		self.val_index = {}
 
+		for name, ser in series.items():
+			self.add_series(name, ser)
+
+
+	@staticmethod
+	def searchInsert(nums: List[int], target: int):
+		if len(nums) == 0:
+			return 0
+
+		left, right = 0, len(nums) - 1
+		
+		while left < right:
+			mid = (left + right) // 2
+			if nums[mid] < target:
+				left = mid + 1
+			elif nums[mid] > target:
+				right = mid - 1
+			else:
+				return mid
+
+		if nums[left] >= target:
+			return left
+		else:
+			return left + 1
+
+	def add_hash_index (self, name, series):
+		mini = min(series.x)
+		maxi = max(series.x)
+
+		ind_mini = self.searchInsert(self.hash_index, mini)
+		ind_maxi = self.searchInsert(self.hash_index, maxi)
+
+		to_add = [(ind_mini, mini), (ind_maxi, maxi)]
+
+		for ind, add in to_add:
+			#shouldn't this be a set then?
+			if self.hash_index[ind] != add:
+				self.hash_index.insert(ind, add)
+		
+			if add in self.val_index:
+				self.val_index.append(name)
+
+			else:
+				self.val_index[add] = [name]
+				if 0 < ind < len(self.hash_index)-1:
+					prev = self.hash_index[ind - 1]
+					aft = self.hash_index[ind + 1]
+
+					#get intersection between the previous and next point to ensure the point contain all series
+					self.val_index[add].extend(list( set(self.val_index[prev]) & set(self.val_index[aft]) ))
+
+
+		for index in range(ind_mini+1, ind_maxi):
+			self.val_index[self.hash_index[index]].append(name)
+
+	def add_series (self, name, series):
+		if name in self.series.keys():
+				self.series [name].append((series, min(series.x), max(series.x)))
+
+			else:
+				self.series [name] = [(series, min(series.x), max(series.x))]
+
+			self.add_hash_index(name, series)
+
+	def get_point_series (self, ind, name):
+		ser, mini, maxi = self.series[name]
+
+		if mini <= ind <= maxi:
+			return ser.get_point_index(ind)
+
+		else:
+			return None
+
+	def get_point_all (self, ind):
+		nearest_larger = self.searchInsert(self.hash_index, ind)
+		if nearest_larger == ind:
+			names = self.val_index[ self.hash_index[ind] ]
+		else:
+
+			names = list( set(self.val_index[self.hash_index[nearest_larger - 1]]) & set(self.val_index[self.hash_index[nearest_larger]]))
+
+		res = {}
+
+		for name in names:
+			res[name] = self.get_point_series(ind, name)
+
+		return res
+		
+
+
+
+
+
+		
 
 
